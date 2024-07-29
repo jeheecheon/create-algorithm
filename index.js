784c8cd2a1dbd06e67cb89ee4cd2b2fd735c33a8
Jehee Cheon <jeheecheon@gmail.com>
2024-07-29 12:38:45 +0900

    chore: 초기 템플릿 업로드

---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+
+// Get the destination path
+const destPath = process.argv[2] || ".";
+
+// Copy template files to destination
+function copyTemplateFiles(srcDir, destDir) {
+  fs.readdirSync(srcDir).forEach((file) => {
+    const srcFile = path.join(srcDir, file);
+    const destFile = path.join(destDir, file);
+    if (fs.lstatSync(srcFile).isDirectory()) {
+      fs.mkdirSync(destFile, { recursive: true });
+      copyTemplateFiles(srcFile, destFile);
+    } else {
+      fs.copyFileSync(srcFile, destFile);
+    }
+  });
+}
+
+// Create destination directory if it doesn't exist
+if (!fs.existsSync(destPath)) {
+  fs.mkdirSync(destPath, { recursive: true });
+}
+
+// Copy template files
+const templateDir = path.join(__dirname, "template");
+copyTemplateFiles(templateDir, destPath);
+
+// Install dependencies
+console.log("Installing dependencies...");
+execSync("yarn install", { stdio: "inherit", cwd: destPath });
+
+console.log("Project setup complete!");
---
+++ template/src/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Input
+ */
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./src/input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+/**
+ * Solution
+ */
