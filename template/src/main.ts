7073ed1da20e568cd44bbe56ddc4054d9d4578e8
jeheecheon <jeheecheon@gmail.com>
2024-07-31 09:41:12 +0900

    feat: main.ts 템플릿 코드 수정

--- template/src/main.ts
+++ template/src/main.ts
@@ -2,7 +2,7 @@
  * Input
  */
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./src/input.txt";
-const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const input: string[] = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
 /**
  * Solution
